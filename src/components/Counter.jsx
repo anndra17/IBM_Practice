22005f82b76122d47bd3bd591a2f376671a32ab6
BiancaStreuneanu <148076488+BiancaStreuneanu@users.noreply.github.com>
2024-07-12 13:19:30 +0300

    modified buttonsx2

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,33 +1,32 @@
-//src/components/Counter.js
 import React from "react";
 import { connect } from "react-redux";
 
-const Counter = ({ x ,y , incrementX, decrementX,incrementY, decrementY}) => {
-  console.log(x,y);
+const Counter = ({ x, y, incrementX, decrementX, incrementY, decrementY }) => {
+  console.log(x, y);
   return (
     <div>
       <p className="counter_title">Player position: X={x} Y={y}</p>
-      <button className="button" onClick={decrementY}>
-        UP
-      </button>
-      <button className="button" onClick={decrementX}>
-        LEFT
-      </button>
-      <button className="button" onClick={incrementY}>
-        DOWN
-      </button>
-      <button className="button" onClick={incrementX}>
-        RIGHT
-      </button>
+      <div className="controls">
+        <button className="button up" onClick={decrementY}>
+          UP
+        </button>
+        <button className="button left" onClick={decrementX}>
+          LEFT
+        </button>
+        <button className="button down" onClick={incrementY}>
+          DOWN
+        </button>
+        <button className="button right" onClick={incrementX}>
+          RIGHT
+        </button>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
-  //  Use 'counter: state.counter.counter' and replace the above line if you are using combineReducers to ensure that 'counter' matches the correct key in your store.
-   x: state.x,
-   y: state.y
-
+  x: state.x,
+  y: state.y
 });
 
 const mapDispatchToProps = (dispatch) => ({
